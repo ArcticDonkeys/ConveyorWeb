8d35812690f0de964031a1eaeb45618d58676cf4
onrylmz <onur.yilmzz@gmail.com>
2015-03-26 20:24:51 +0200

    Map'den mouse position bilgisi alındı.

--- src/main/webapp/js/mainpage.js
+++ src/main/webapp/js/mainpage.js
@@ -14,12 +14,21 @@ function initializeMap()
         zoom: 11,
         mapTypeId: google.maps.MapTypeId.HYBRID
     }
-    var map = new google.maps.Map(mapCanvas,mapOptions)
+    var map = new google.maps.Map(mapCanvas,mapOptions);
+    
+    google.maps.event.addListener(map, 'click', function (e){
+        var latitude = document.getElementById('latitude');
+        var longitude = document.getElementById('longitude');
+        
+        latitude.value = e.latLng.lat();
+        longitude.value = e.latLng.lng();
+    });
 }
 
 $(document).ready(function(){
     
    google.maps.event.addDomListener(window, 'load', initializeMap);
+   
    
 });
 
