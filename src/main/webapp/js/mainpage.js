64358522700155666fc2c20be0af47b654163095
onrylmz <onur.yilmzz@gmail.com>
2015-03-27 10:09:16 +0200

    Haritaya double click marker eklendi.

--- src/main/webapp/js/mainpage.js
+++ src/main/webapp/js/mainpage.js
@@ -4,6 +4,43 @@
  * and open the template in the editor.
  */
 
+var markers = [];
+
+function removeOtherMarkers(){
+    for(var i = 0; i < markers.length; i++)
+    {
+        markers[i].setMap(null);
+    }
+    
+    markers = [];
+}
+
+function addMarker(e, map){
+    
+    var marker = new google.maps.Marker({
+            position: e.latLng,
+            map: map
+        });
+    
+    map.setZoom(10);
+
+    markers.push(marker);
+}
+
+function doubleClickListener(map){
+    google.maps.event.addListener(map, 'dblclick', function (e){
+        var latitude = document.getElementById('latitude');
+        var longitude = document.getElementById('longitude');
+        
+        latitude.value = e.latLng.lat();
+        longitude.value = e.latLng.lng();
+        
+        removeOtherMarkers(); /*Marker koymadan önce haritadan diğer markerları siler.*/
+        
+        addMarker(e, map); /*Markerı bu fonksiyon oluşturur*/
+        
+    });
+}
 
 
 function initializeMap()
@@ -16,13 +53,7 @@ function initializeMap()
     }
     var map = new google.maps.Map(mapCanvas,mapOptions);
     
-    google.maps.event.addListener(map, 'click', function (e){
-        var latitude = document.getElementById('latitude');
-        var longitude = document.getElementById('longitude');
-        
-        latitude.value = e.latLng.lat();
-        longitude.value = e.latLng.lng();
-    });
+    doubleClickListener(map); /*Haritada double click'i handle eder.*/
 }
 
 $(document).ready(function(){
